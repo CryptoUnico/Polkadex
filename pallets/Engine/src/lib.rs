9b4d6f899de1f7b2151f2d24e364a45ded29168e
Gautham J <Gauthamastro@gmail.com>
2021-03-01 12:30:08 +0530

    Implementing balances

--- pallets/Engine/src/lib.rs
+++ pallets/Engine/src/lib.rs
@@ -6,9 +6,13 @@ use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch,
                     traits::Get,
                     weights::Weight};
 use frame_system::ensure_signed;
-use sp_runtime::traits::{AtLeast32BitUnsigned, IdentifyAccount, MaybeSerializeDeserialize, Member, Verify, Zero};
+use sp_core::Hasher;
+use sp_runtime::app_crypto::sp_core::sr25519;
+use sp_runtime::{DispatchError, ModuleId};
+use sp_runtime::traits::{AtLeast32BitUnsigned, IdentifyAccount, MaybeSerializeDeserialize, Member, Verify, Zero, AccountIdConversion, Saturating};
+use sp_std::collections::btree_map;
 
-use polkadex_primitives::engine::{BalanceState, Commitment, FraudProof, Log, OrderType, SpotTrade, State};
+use polkadex_primitives::engine::{BalanceState, Commitment, FraudProof, Log, OrderType, SpotTrade, State, VerificationErrors};
 
 #[cfg(test)]
 mod mock;
@@ -30,6 +34,8 @@ pub trait Config: frame_system::Config {
     type AssetID: Ord + Encode + Decode + Clone + Debug;
     /// Dispute Period
     type DisputePeriod: Get<<Self as frame_system::Config>::BlockNumber>;
+    /// Maximum Exchange Fee
+    type MaxTradingFee: Get<<Self as Config>::Balance>;
 }
 
 decl_storage! {
@@ -88,59 +94,193 @@ decl_module! {
 		#[weight = 0]
 		pub fn submit_commitment(origin, commitment: Commitment<T::AccountId, T::Balance, T::Signature, T::AssetID>) -> dispatch::DispatchResult {
 			let provider = ensure_signed(origin)?;
-			if <Providers<T>>::contains_key(provider) && !<Commitments<T>>::contains_key(<frame_system::Module<T>>::block_number()){
+			if <Providers<T>>::contains_key(provider) && !<Commitments<T>>::contains_key(<frame_system::Module<T>>::block_number()){ //
 			    <Commitments<T>>::insert(<frame_system::Module<T>>::block_number(), commitment);
 			}
 			Ok(())
+		}
+
+		fn offchain_worker(now: T::BlockNumber){
+		    Self::execute_offchain_worker(now);
 		}
 	}
 }
 
 impl<T: Config> Module<T> {
+    /// Stores fees collected from trades
+    pub fn get_exchange_account() -> T::AccountId {
+        ModuleId(*b"exchange").into_account()
+    }
+
     fn finalize_commitment(now: T::BlockNumber) -> Weight {
         if now - T::DisputePeriod::get() >= 0.into() && !<FraudProofs<T>>::contains_key(now - T::DisputePeriod::get()) {
             let commitment: Commitment<T::AccountId, T::Balance, T::Signature, T::AssetID> = <Commitments<T>>::take(now - T::DisputePeriod::get());
             let mut prev_state: State<T::AssetID, T::Balance, T::AccountId> = <Balances<T>>::get();
             for state in 0..commitment.final_state.len() { // FIXME: Bound this loop.
-                prev_state.balances.insert(commitment.final_state[state].0.clone(),commitment.final_state[state].1.clone());
+                prev_state.balances.insert(commitment.final_state[state].0.clone(), commitment.final_state[state].1.clone());
             }
+            prev_state.nonce = commitment.final_nonce;
             <Balances<T>>::put(prev_state);
-            return Weight::zero() // FIXME: Implement proper weight based on number of loops
+            return Weight::zero(); // FIXME: Implement proper weight based on number of loops
         }
         Weight::zero()
     }
-    // fn verify_signatures(maker: &Order<T::Balance, T::AccountId, T::Hash>,
-    //                      taker: &Order<T::Balance, T::AccountId, T::Hash>,
-    //                     maker_msg: &T::Hash,
-    //                     taker_msg: &T::Hash) -> bool {
-    //
-    //     // sr25519 always expects a 64 byte signature.
-    //     // ensure!(maker.signature.len() == 64 && taker.signature.len() == 64, Error::<T>::InvalidSignature);
-    //     let maker_signature: sr25519::Signature = sr25519::Signature::from_slice(&maker.signature).into();
-    //
-    //     // In Polkadot, the AccountId is always the same as the 32 byte public key.
-    //     let maker_account_bytes: [u8; 32] = account_to_bytes(&maker.trader).unwrap();
-    //     let maker_public_key = sr25519::Public::from_raw(maker_account_bytes);
-    //
-    //     let taker_signature: sr25519::Signature = sr25519::Signature::from_slice(&taker.signature).into();
-    //
-    //     // In Polkadot, the AccountId is always the same as the 32 byte public key.
-    //     let taker_account_bytes: [u8; 32] = account_to_bytes(&taker.trader).unwrap();
-    //     let taker_public_key = sr25519::Public::from_raw(taker_account_bytes);
-    //
-    //     taker_signature.verify(&taker_msg.encode()[..], &taker_public_key) && maker_signature.verify(&maker_msg.encode()[..], &maker_public_key)
-    //
-    //
-    // }
+
+    fn execute_offchain_worker(now: T::BlockNumber) {
+        let target_block: T::BlockNumber = now - T::DisputePeriod::get();
+        if target_block >= 0.into() && <Commitments<T>>::contains_key(target_block) {
+            let target_commitment: Commitment<T::AccountId, T::Balance, T::Signature, T::AssetID> = <Commitments<T>>::get(target_block);
+            let mut prev_state: State<T::AssetID, T::Balance, T::AccountId> = <Balances<T>>::get();
+            for log in target_commitment.logs {
+                match log {
+                    Log::SpotSettlement(maker, taker, fee,  nonce) => {
+                        // FIXME: Verify the nonces of each trade
+                        let maker_msg: T::Hash = (maker.price, maker.amount, maker.order_type, maker.base_asset, maker.quote_asset, maker.nonce).using_encoded(<T as frame_system::Config>::Hashing::hash);
+                        let taker_msg: T::Hash = (taker.price, taker.amount, taker.order_type, taker.base_asset, taker.quote_asset, taker.nonce).using_encoded(<T as frame_system::Config>::Hashing::hash);
+                        if Self::verify_signatures(&maker, &taker, maker_msg, taker_msg) && Self::verify_nonce(nonce, prev_state.nonce) {
+                            Self::verify_trades_and_update(&maker, &taker, &prev_state.balances, fee);
+                        } else {
+                            Self::raise_dispute(VerificationErrors::OrderSignatureFailure);
+                        }
+                    }
+                    Log::Deposit(trader, assetID, amount, nonce) => {
+                        if Self::verify_nonce(nonce, prev_state.nonce) {
+                            // TODO: Update State
+
+                            prev_state.nonce = nonce;
+                        }
+                    }
+                    Log::Withdrawal(trader, assetID, amount, nonce) => {
+                        if Self::verify_nonce(nonce, prev_state.nonce) {
+                            // TODO: Update State
+
+                            prev_state.nonce = nonce;
+                        }
+                    }
+                }
+            }
+            if !Self::verify_state(&prev_state, &target_commitment.final_state) {
+                Self::raise_dispute(VerificationErrors::InvalidFinalState);
+            }
+        }
+    }
+
+    /// BTC/USD: BTC is the base asset, USD is the quote asset.
+    /// Price is defined in terms of quote asset
+    /// Amount is defined in terms of base asset
+    fn verify_trades_and_update(maker: &SpotTrade<T::AccountId, T::Balance, T::Signature, T::AssetID>,
+                                taker: &SpotTrade<T::AccountId, T::Balance, T::Signature, T::AssetID>,
+                                state: &mut btree_map::BTreeMap<(T::AccountID, T::AssetID), BalanceState<T::Balance>>,
+                                fee: T::Balance) {
+        if fee > T::MaxTradingFee::get(){
+            Self::raise_dispute(VerificationErrors::InvalidFee);
+            return
+        }
+
+        match (maker.order_type, taker.order_type) {
+            /// These are the possible combinations for a given maker and taker pair
+            (OrderType::BidLimit, OrderType::AskLimit) => {
+                if taker.price <= maker.price && taker.amount <= maker.amount {
+                    // Calculate Quote asset
+                    // Calculate Base asset
+                    // Calculate Fees
+                    let quote_amt: T::Balance = taker.price*taker.amount; // Given to maker
+                    let base_amt: T::Balance = taker.amount - taker.amount*fee; // Given to taker
+                    let fees_collected: T::Balance = base_amt*fee;  // Given to exchange
+                    // Maker Balances
+                    let maker_quote: T::Balance = state.get_mut(&(&maker.trader,&maker.quote_asset)).unwrap();
+                    let maker_base: T::Balance = state.get_mut(&(&maker.trader,&maker.base_asset)).unwrap();
+                    // Taker Balances
+                    let taker_quote: T::Balance = state.get_mut(&(&taker.trader,&taker.quote_asset)).unwrap();
+                    let taker_base: T::Balance = state.get_mut(&(&taker.trader,&taker.base_asset)).unwrap();
+                    // Exchange Balances
+                    let exchange_base: T::Balance = state.get_mut(&(Self::get_exchange_account(),&taker.base_asset));
+
+                    // Update the balances
+
+
+                } else {
+                    Self::raise_dispute(VerificationErrors::OrderParamCheckFailed);
+                }
+            }
+            (OrderType::AskLimit, OrderType::BidLimit) => {
+                if maker.price <= taker.price && taker.amount <= maker.amount {
+                    // TODO: Calculate Quote asset
+                    // TODO: Calculate Base asset
+                    // TODO: Calculate Fees
+                    // TODO: Transfer amounts
+                } else {
+                    Self::raise_dispute(VerificationErrors::OrderParamCheckFailed);
+                }
+            }
+            (OrderType::BidLimit, OrderType::AskMarket) => {
+                if taker.amount <= maker.amount {
+                    // TODO: Calculate Quote asset
+                    // TODO: Calculate Base asset
+                    // TODO: Calculate Fees
+                    // TODO: Transfer amounts
+                } else {
+                    Self::raise_dispute(VerificationErrors::OrderAmountFailure);
+                }
+            }
+            (OrderType::AskLimit, OrderType::BidMarket) => {
+                if taker.amount <= maker.amount {
+                    // TODO: Calculate Quote asset
+                    // TODO: Calculate Base asset
+                    // TODO: Calculate Fees
+                    // TODO: Transfer amounts
+                } else {
+                    Self::raise_dispute(VerificationErrors::OrderAmountFailure);
+                }
+            }
+            _ => {
+                Self::raise_dispute(VerificationErrors::InvalidOrderCombination);
+            } /// This shouldn't execute ever.
+        }
+    }
+
+    fn raise_dispute(error: VerificationErrors) {
+        unimplemented!();
+    }
+
+    fn verify_state(computed_state: &State<T::AssetID, T::Balance, T::AccountId>,
+                    given_state: &State<T::AssetID, T::Balance, T::AccountId>) -> bool {
+        unimplemented!();
+    }
+
+    fn verify_signatures(maker: &SpotTrade<T::AccountId, T::Balance, T::Signature, T::AssetID>,
+                         taker: &SpotTrade<T::AccountId, T::Balance, T::Signature, T::AssetID>,
+                         maker_msg: &T::Hash,
+                         taker_msg: &T::Hash) -> bool {
+        // sr25519 always expects a 64 byte signature.
+        ensure!(maker.signature.len() == 64 && taker.signature.len() == 64, Error::<T>::InvalidSignature);
+        let maker_signature: sr25519::Signature = sr25519::Signature::from_slice(&maker.signature).into();
+
+        // In Polkadot, the AccountId is always the same as the 32 byte public key.
+        let maker_account_bytes: [u8; 32] = account_to_bytes(&maker.trader).unwrap();
+        let maker_public_key = sr25519::Public::from_raw(maker_account_bytes);
+
+        let taker_signature: sr25519::Signature = sr25519::Signature::from_slice(&taker.signature).into();
+
+        // In Polkadot, the AccountId is always the same as the 32 byte public key.
+        let taker_account_bytes: [u8; 32] = account_to_bytes(&taker.trader).unwrap();
+        let taker_public_key = sr25519::Public::from_raw(taker_account_bytes);
+
+        taker_signature.verify(&taker_msg.encode()[..], &taker_public_key) && maker_signature.verify(&maker_msg.encode()[..], &maker_public_key)
+    }
+
+    fn verify_nonce(nonce: u128, prev_nonce: u128) -> bool {
+        nonce == prev_nonce + 1
+    }
 }
 
 // This function converts a 32 byte AccountId to its byte-array equivalent form.
-// fn account_to_bytes<AccountId>(account: &AccountId) -> Result<[u8; 32], DispatchError>
-//     where AccountId: Encode,
-// {
-//     let account_vec = account.encode();
-//     ensure!(account_vec.len() == 32, "AccountId must be 32 bytes.");
-//     let mut bytes = [0u8; 32];
-//     bytes.copy_from_slice(&account_vec);
-//     Ok(bytes)
-// }
+fn account_to_bytes<AccountId>(account: &AccountId) -> Result<[u8; 32], DispatchError>
+    where AccountId: Encode,
+{
+    let account_vec = account.encode();
+    ensure!(account_vec.len() == 32, "AccountId must be 32 bytes.");
+    let mut bytes = [0u8; 32];
+    bytes.copy_from_slice(&account_vec);
+    Ok(bytes)
+}
--- primitives/src/engine.rs
+++ primitives/src/engine.rs
@@ -13,24 +13,38 @@ pub enum OrderType {
     AskMarket,
 }
 
+#[derive(Encode, Decode, Copy, Clone, PartialEq, Eq, PartialOrd, RuntimeDebug)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub enum VerificationErrors{
+    OrderAmountFailure,
+    OrderPriceFailure,
+    InvalidOrderCombination,
+    OrderParamCheckFailed,
+    OrderSignatureFailure,
+    InvalidFinalState,
+    InvalidFee
+
+}
+
 
 #[derive(Encode, Decode, Copy, Clone, PartialEq, Eq, PartialOrd, RuntimeDebug)]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 pub struct SpotTrade<AccountID: Ord + Default, Balance, Signature, AssetID: Ord> {
-    trader: AccountID,
-    price: Balance,
-    amount: Balance,
-    order_type: OrderType,
-    base_asset: AssetID,
-    quote_asset: AssetID,
-    nonce: u128,
-    signature: Signature,
+    pub trader: AccountID,
+    pub price: Balance,
+    pub amount: Balance,
+    pub order_type: OrderType,
+    pub base_asset: AssetID,
+    pub quote_asset: AssetID,
+    pub nonce: u128,
+    pub signature: Signature,
 }
 
 #[derive(Encode, Decode, Copy, Clone, PartialEq, Eq, PartialOrd, RuntimeDebug)]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 pub enum Log<AccountID: Ord + Default, Balance, Signature, AssetID: Ord> {
-    SpotSettlement(SpotTrade<AccountID, Balance, Signature, AssetID>, SpotTrade<AccountID, Balance, Signature, AssetID>, u128),
+    /// SpotSettlement(maker,taker, fee as fraction, nonce)
+    SpotSettlement(SpotTrade<AccountID, Balance, Signature, AssetID>, SpotTrade<AccountID, Balance, Signature, AssetID>, Balance, u128),
     Withdrawal(AccountID, AssetID, Balance, u128),
     Deposit(AccountID, AssetID, Balance, u128),
 }
@@ -45,13 +59,17 @@ pub struct BalanceState<Balance> {
 #[derive(Encode, Decode, Clone, PartialEq, Eq, PartialOrd, RuntimeDebug)]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 pub struct State<AssetID: Ord, Balance, AccountID: Ord + Default> {
-    pub balances: btree_map::BTreeMap<(AccountID, AssetID), BalanceState<Balance>>
+    pub balances: btree_map::BTreeMap<(AccountID, AssetID), BalanceState<Balance>>,
+    pub nonce: u128,
+    pub nonces: btree_map::BTreeMap<AccountID,u128>
 }
 
 impl<AssetID: Ord, Balance, AccountID: Ord + Default> Default for State<AssetID, Balance, AccountID> {
     fn default() -> Self {
         State {
-            balances: btree_map::BTreeMap::new()
+            balances: btree_map::BTreeMap::new(),
+            nonce: 0,
+            nonces: btree_map::BTreeMap::new()
         }
     }
 }
@@ -60,7 +78,7 @@ impl<AssetID: Ord, Balance, AccountID: Ord + Default> Default for State<AssetID,
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 pub struct Commitment<AccountID: Ord + Default, Balance, Signature, AssetID: Ord> {
     pub logs: Vec<Log<AccountID, Balance, Signature, AssetID>>,
-    pub final_state: Vec<((AccountID, AssetID), BalanceState<Balance>)>,
+    pub final_state: Vec<((AccountID, AssetID), BalanceState<Balance>)>
 }
 
 impl<AccountID: Ord + Default, Balance, Signature, AssetID: Ord> Default for Commitment<AccountID, Balance, Signature, AssetID> {
